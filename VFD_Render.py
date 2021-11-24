489be688649dff1312b5e4e3ae014f4ec13f5ea9
Tom Oldbury <toldbury@gmail.com>
2021-11-24 23:15:11 +0000

    testing/debugging

--- VFD_Render.py
+++ VFD_Render.py
@@ -336,8 +336,8 @@ class VFD(object):
             #self._send_command(b"Hello\r\n")
             self.clear()
             #self.stream_out(self.vfd_surf, 10 + (self.frame % 60), 0, 20 + (self.frame % 60), 0)
-            self.stream_out(self.vfd_surf, 0, 0, 111, 7)
             #self.stream_out(self.vfd_surf, 0, 0, 111, 7)
+            self.stream_out(self.vfd_surf, 0, 8, 111, 7)
 
         # we also push it to the window and wait for the vsync
         if self.window != None:
