763c47b7956d02f4292f066ef5df5c411bd30ad8
Tom Oldbury <toldbury@gmail.com>
2021-11-27 23:16:13 +0000

    Change the damage algorithm

--- VFD_Render.py
+++ VFD_Render.py
@@ -211,7 +211,7 @@ class VFD(object):
             print("runs:", runs)
             
             for run in runs:
-                run_ranges.append((run[0], run[-1]))
+                run_ranges.append((run[0], clamp(run[-1] + 1, 0, VFD_WIDTH - 1)))
             
             rows[yn] = run_ranges
             yp += DAMAGE_ROW_HEIGHT
