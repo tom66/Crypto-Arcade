10662b918be5fd9c264f64b6188ea50fd4429d9a
Tom Oldbury <toldbury@gmail.com>
2021-11-27 18:53:02 +0000

    Change the damage algorithm

--- VFD_Render.py
+++ VFD_Render.py
@@ -5,6 +5,8 @@ pygame.init()
 DEMO_SCALE = 8
 VFD_WIDTH = 112
 VFD_HEIGHT = 16
+DAMAGE_ROWS = 2
+DAMAGE_ROW_HEIGHT = 8
 
 COL_WHITE = (255, 255, 255)
 COL_BLACK = (0, 0, 0)
@@ -19,11 +21,17 @@ except ImportError:
     GPIO = None
     AM_A_PI = False
 
+def clamp(val, minval, maxval):
+    if val < minval: return minval
+    if val > maxval: return maxval
+    return val
+
 class VFD(object):
     window = None
     vfd_surf = None
     last_vfd_surf = None
     frame = 0
+    damage_rows = []
     
     def __init__(self, render_window):
         if render_window:
@@ -57,6 +65,12 @@ class VFD(object):
         self.re_init()
         self.set_window(0)
         self.clear()
+
+        # Reset the damage arrays
+        self.damage_rows = []
+        
+        for n in range(DAMAGE_ROWS):
+            self.damage_rows.append([0] * VFD_WIDTH)
 
     def _wait_sbusy(self):
         # wait for GPIO to be free.  Modestly-inefficiently spin the CPU on this
@@ -94,176 +108,88 @@ class VFD(object):
                     break
         
         self._wait_sbusy()
-                    
+
+    def add_damage_region(self, x0, y0, x1, y1):
+        x0 = clamp(x0, 0, VFD_WIDTH)
+        x1 = clamp(x1, 0, VFD_WIDTH)
+
+        if x0 > x1:
+            x1, x0 = x0, x1
+
+        y0 = clamp(y0, 0, VFD_HEIGHT)
+        y1 = clamp(y1, 0, VFD_HEIGHT)
+
+        if y0 > y1:
+            y1, y0 = y0, y1
+        
+        damage_start = int(y0 / DAMAGE_ROW_HEIGHT)
+        damage_end = int(y1 / DAMAGE_ROW_HEIGHT)
+
+        # Two rows is all we support for now...
+        if damage_start != damage_end:
+            for x in range(x0, x1):
+                damage_rows[0][x] = 1
+                damage_rows[1][x] = 1
+        else:
+            for x in range(x0, x1):
+                damage_rows[damage_start][x] = 1
+
+    def clear_damage(self):
+        for n in range(DAMAGE_ROWS):
+            for x in range(x0, x1):
+                self.damage_rows[n][x] = 0
+    
     def text(self, font, x, y, str_, col=COL_WHITE):
         surf = font.render(str_, False, col)
+        pos = (x, y)
+        self.add_damage_region(pos[0], pos[1], pos[0] + surf.get_width(), pos[1] + surf.get_height())
         self.vfd_surf.blit(surf, dest=(x, y))
 
     def text_right(self, font, x, y, str_, col=COL_WHITE):
         surf = font.render(str_, False, col)
-        self.vfd_surf.blit(surf, dest=(self.vfd_surf.get_width() - surf.get_width() - x, y))
+        pos = (self.vfd_surf.get_width() - surf.get_width() - x, y)
+        self.add_damage_region(pos[0], pos[1], pos[0] + surf.get_width(), pos[1] + surf.get_height())
+        self.vfd_surf.blit(surf, dest=pos)
 
     def line(self, x0, y0, x1, y1, w, col=COL_WHITE):
+        self.add_damage_region(x0 - w + 1, y0 - w + 1, x1 + w - 1, y1 + w - 1)  # Approximate bounding box with line width
         pygame.draw.line(self.vfd_surf, col, (x0, y0), (x1, y1), w)
 
     def circle_inverse(self, x0, y0, r, w, col=COL_WHITE):
+        self.add_damage_region(x0 - r - w + 1, y0 - r - w + 1, x0 + r + w - 1, y0 + r + w - 1)
         pygame.draw.circle(self.inv_surf, col, (x0, y0), r, w)
-    
-    def arc_inverse(self, x0, y0, r, w, sa, ea, col=COL_WHITE):
-        sa *= 3.14159/180.0
-        ea *= 3.14159/180.0
-        pygame.draw.arc(self.inv_surf, col, pygame.Rect((x0, y0), (r, r)), sa, ea, w)
 
     def calculate_damage_list(self):
-        # List used to colorise the clusters for debugging
-        t0 = time.time()
+        # For each damage row, try to find a long contiguous string of 1s, indicating
+        # a section is damaged.
         
-        cluster_cols = []
-        for n in range(63, 255, 15):
-            cluster_cols.append([n, 0, 0])
-            cluster_cols.append([0, n, 0])
-            cluster_cols.append([0, 0, n])
-        working_cols = copy.copy(cluster_cols)
+        max_spacing = 4  # If less than 4 between this and adjacent runs, then break the runs up.
+        rows = [[]] * DAMAGE_ROWS
+        yn = 0
         
-        # Check pixels from left to right and find the first non-matching pixel
-        a = pygame.surfarray.pixels3d(self.vfd_surf)
-        b = pygame.surfarray.pixels3d(self.last_vfd_surf)
-        damage = []
-        whole = False
-        
-        for y in range(0, VFD_HEIGHT):
-            if whole: break
-            for x in range(0, VFD_WIDTH):
-                if whole: break
-                if (a[x][y].all() != b[x][y].all()):
-                    if (x, y) not in damage:
-                        damage.append((x, y))
-
-                        # Threshold >400 pixels changed, send the whole frame
-                        if len(damage) > 400:
-                            whole = True
-
-        if whole:
-            pygame.draw.rect(self.damage_surf, cluster_cols[0], (0, 0, VFD_WIDTH, VFD_HEIGHT))
-            return [(0, 0, VFD_WIDTH, VFD_HEIGHT)]
-
-        # Work through the damage list.  Arbitrarily pick the first pixel,
-        # and try to find neighbours within a search_width x search_height block.
-        # If a pixel is found, remove it from the damage list.  Add the bounding
-        # box for damage pixels to the 'touched' list.  Iterate until there are
-        # no pixels left in the damage list.
-        clusters = []
-        touched = []
-        search_width = 8
-        search_height = 8
-        bboxes = []
-
-        #print("There are %d damaged pixels" % len(damage))
-        damage.sort(key=lambda x:(x[0]-x[1]))
-        
-        while (len(damage) > 0):
-            ref = damage.pop()
+        for row in self.damage_rows:
+            last_one = None
+            runs = []
+            run = []
             
-            if ref not in touched:
-                touched.append(ref)
-                cluster = [ref]
-            else:
-                continue
+            for n, x in enumerate(row):
+                if x:
+                    if (n - last_one) > 4:
+                        runs += run
+                        run = [n]
+                    else:
+                        run.append(n)
 
-            for adj in damage:
-                # ignore touched pixels
-                if adj in touched:
-                    #print("Skipping", adj, "as already touched")
-                    continue
-                
-                # trivial test: near to start coord?
-                if abs(adj[0] - ref[0]) < search_width and abs(adj[1] - ref[1]) < search_height:
-                    cluster.append(adj)
-                    touched.append(adj)
-                    damage.remove(adj) # O(n); could be O(1) at best
-                else:
-                    # if the pixel is close to other damage pixels, it becomes part of the same cluster
-                    close = False
-                    for pp in cluster:
-                        #print("Dist:", abs(adj[0] - pp[0]), abs(adj[1] - pp[1]), adj, pp)
-                        if abs(adj[0] - pp[0]) < search_width and abs(adj[1] - pp[1]) < search_height:
-                            close = True
-                            break
-
-                    if close:
-                        touched.append(adj)
-                        cluster.append(adj)
-                        damage.remove(adj) # O(n); could be O(1) at best
-
-            #print("Got cluster of %d pixels" % len(cluster))
-            #pprint.pp(touched)
-
-            try:
-                col = working_cols.pop()
-            except IndexError:
-                # ran out of colours, re-initialise list
-                working_cols = copy.copy(cluster_cols)
-
-            # find the bounding box of the cluster.  clusters must contain at least two pixels,
-            # and cluster Y positions must start at 0 or 8
-            if len(cluster) >= 2:
-                x0 = cluster[0][0]
-                y0 = cluster[0][1]
-                x1 = cluster[1][0]
-                y1 = cluster[1][1]
-                
-                for p in cluster:
-                    if x0 > p[0]:
-                        x0 = p[0]
-                    if x1 < p[0]:
-                        x1 = p[0]
-                    if y0 > p[1]:
-                        y0 = p[1]
-                    if y1 < p[1]:
-                        y1 = p[1]
-
-                if (y0 % 8) != 0:
-                    y0 = y0 & ~0x07
-                    
-                if (y1 % 8) != 0:
-                    y1 = (y1 + 8) & ~0x07
-
-                #pygame.draw.rect(self.damage_surf, col, (x0, y0, x1 - x0 + 1, y1 - y0 + 1))
-                bboxes.append((x0, y0, x1, y1))
-
-                # 'touch' all the pixels in this box
-                for x in range(x0, x1 + 1):
-                    for y in range(y0, y1 + 1):
-                        self.damage_surf.set_at((x, y), col)
-                        
-                        if (x, y) not in touched:
-                            touched.append((x, y))
-                            
-                        try:
-                            damage.remove((x, y))
-                        except:
-                            pass
-                            
-            elif len(cluster) == 1:
-                # not really a cluster is it, still needs to be handled
-                x0 = cluster[0][0]
-                y0 = cluster[0][1] & ~0x07
-                y1 = y0 + 8
-
-                for y in range(y0, y1):
-                    self.damage_surf.set_at((x0, y), col)
-                    touched.append((x, y))
-                
-                    try:
-                        damage.remove((x, y))
-                    except:
-                        pass
-
-                bboxes.append((x0, y0, x0, y1))
-
-        t1 = time.time()
-        #print("BBOX solve in %d ms" % ((t1 - t0) * 1000))
-        return bboxes
+            run_ranges = []
+            
+            for run in runs:
+                run_ranges.append((run[0], run[-1]))
+                pygame.display.rect(self.damage_surf, (255, 0, 0), (run[0], yn * DAMAGE_ROW_HEIGHT, run[-1] - run[0], DAMAGE_ROW_HEIGHT))
+            
+            rows[yn] = run_ranges
+            yn += 1
+        
+        print(rows)
     
     def set_window(self, win):
         assert(win >= 0 and win <= 4)
@@ -330,7 +256,8 @@ class VFD(object):
         b = pygame.surfarray.pixels3d(self.inv_surf)
         self.vfd_surf = pygame.surfarray.make_surface(a ^ b)
         
-        #self.calculate_damage_list()
+        self.calculate_damage_list()
+        self.clear_damage()
         
         # here we stream the surface to the Noritake VFD display
         if AM_A_PI:
