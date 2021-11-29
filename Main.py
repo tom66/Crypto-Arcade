e5c114df8b132633a53bdca49cd9a3e5973d43df
Tom Oldbury <toldbury@gmail.com>
2021-11-29 22:11:37 +0000

    Transitions

--- Main.py
+++ Main.py
@@ -234,19 +234,20 @@ class Main(object):
             self.vfd.text(self.small_font, 0, 0, c_data._fname)
 
         # Show alternating text
-        f_sub = int(self.f % 600)
+        f_sub = int(self.f % 650)
+        print("vusd", c_data.volumeUSD)
         
-        if f_sub < 50:
+        if f_sub < 75:
             self.vfd.text(self.small_font, 0, 9, "24h")
-        elif f_sub < 100:
-            self.vfd.text(self.small_font, 0, 9, "Chg$")
         elif f_sub < 150:
+            self.vfd.text(self.small_font, 0, 9, "Chg$")
+        elif f_sub < 300:
             self.vfd.text(self.small_font, 0, 9, sign_fmt_dec("", "%", c_data.priceUSDChange24Hr))
-        elif f_sub < 350:
-            self.vfd.text(self.small_font, 0, 9, "24h")
-        elif f_sub < 400:
-            self.vfd.text(self.small_font, 0, 9, "Vol$")
         elif f_sub < 450:
+            self.vfd.text(self.small_font, 0, 9, "24h")
+        elif f_sub < 525:
+            self.vfd.text(self.small_font, 0, 9, "Vol$")
+        elif f_sub < 600:
             self.vfd.text(self.small_font, 0, 9, nosign_fmt_dec("", "", c_data.volumeUSD))
         
         self.vfd.text_right(self.big_font, 0, -4, usd_fmt_nodec(c_data.lastPriceUSD))
