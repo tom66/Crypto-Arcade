9be6f0cd0cf896b206221ba1dfcb30badc970c44
Tom Oldbury <toldbury@gmail.com>
2021-12-12 17:49:14 +0000

    Add startup scripts

--- K99_VFD_shutdown.sh
+++ K99_VFD_shutdown.sh
@@ -1,7 +1,7 @@
 # reset display
 stty -F /dev/ttyS0 115200
 sleep 0.1
-echo "\x1B\x40" > /dev/ttyS0
+echo -n -e "\x1B\x40" > /dev/ttyS0
 sleep 0.1
-echo "\x0C" > /dev/ttyS0
+echo -n -e "\x0C" > /dev/ttyS0
 sleep 0.1
--- S01_VFD_startup.sh
+++ S01_VFD_startup.sh
@@ -1,9 +1,9 @@
 # reset display
 stty -F /dev/ttyS0 115200
 sleep 0.1
-echo "\x1B\x40" > /dev/ttyS0
+echo -n -e "\x1B\x40" > /dev/ttyS0
 sleep 0.1
-echo "\x0C" > /dev/ttyS0
+echo -n -e "\x0C" > /dev/ttyS0
 sleep 0.1
-echo "Booting..." > /dev/ttyS0
+echo -n -e "Booting..." > /dev/ttyS0
 sleep 0.1
